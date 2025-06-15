bc799b4a2f8e7a50e73f2bfce7b05c76d000babd
Roman Raut <romanraut@Romans-MacBook-Pro.local>
2025-06-15 18:07:16 +0545

    Dictionary concpet

---
+++ chapter5/dict.py
@@ -0,0 +1,11 @@
+#Dictionary is used in python to store data in key-value pairs.
+
+marks = {
+    "John": 85,
+    "Roman": 92,
+    "sita": 78,
+    "Juli": 95
+}
+print(marks)
+# To access a value in a dictionary, we can use the key.
+print("Marks of John:", marks["John"])
